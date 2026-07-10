befd6762a756706147cf611f9a8e373cee219b2f
Yashmevada2807 <yashmevada07@gmail.com>
2026-07-10 11:05:26 +0530

    added handlechatRequest controller

---
+++ backend/src/controllers/chatController.controller.js
@@ -0,0 +1,25 @@
+import { asyncHandler } from "../utils/async-handler.js";
+import { ApiRespose } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { processUserQuery } from "../services/chatService.service.js";
+
+
+const handleChatRequest = asyncHandler(async (req, res) => {
+    const { userInput } = req.body
+
+    if (!userInput || userInput.trim() === "") throw new ApiError(400, "Please enter a valid question or message.")
+    
+    const result = await processUserQuery(userInput)
+
+    return res.json(
+        new ApiRespose(
+            200,
+            result,
+            "user query processed succesfully"
+        )
+    )
+    
+})
+
+
+export { handleChatRequest }
