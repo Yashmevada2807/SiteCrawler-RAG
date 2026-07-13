cf2e23cdc2e4ca29584c681f87db10f31a797a13
Yashmevada2807 <yashmevada07@gmail.com>
2026-07-13 21:55:42 +0530

    creates random crawlId's with the help of uuidv4 and store it in vector db for fetching relevant data of particular user also get userId from frontend

--- backend/src/controllers/crawlingSite.controller.js
+++ backend/src/controllers/crawlingSite.controller.js
@@ -2,15 +2,21 @@ import { ApiRespose } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { crawlingSite } from "../services/crawlingSite.service.js";
+import {v4 as uuidv4} from "uuid"
 
 
 
 const crawlingSiteHandler = asyncHandler(async (req, res) => {
-    const { url } = req.body
+    const { url, userId } = req.body
 
     if (!url) throw new ApiError(400, "Please provide a valid website URL.")
+    const crawlId = uuidv4() 
 
-    const totalFetchedPages = await crawlingSite(url)
+    const totalFetchedPages = await crawlingSite(
+        url,
+        userId,
+        crawlId
+    )
 
     return res.json(
         new ApiRespose(
